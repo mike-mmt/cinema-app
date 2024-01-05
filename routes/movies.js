1a236d3b97c3e2f06af08370710639d929549d00
chillktht <108435079+chillktht@users.noreply.github.com>
2024-01-05 23:05:54 +0100

    Add /movies route

---
+++ routes/movies.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
+const Screening = require("../models/Screening");
+const verifyJWT = require("../utils/verify-jwt-middleware");
+
+router.get("/", verifyJWT, async (req, res, next) => {
+  try {
+    const allMovies = await Movie.find()
+      .where("isCurrentlyScreening")
+      .equals(true)
+      .select("_id title genres mainPhotoId")
+      .exec();
+    return res.status(200).json(allMovies);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
