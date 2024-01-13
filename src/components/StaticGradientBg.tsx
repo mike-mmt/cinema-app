81b59c8a53040a88bf0c707c638ec36e085459db
chillktht <108435079+chillktht@users.noreply.github.com>
2024-01-13 21:40:00 +0100

    added a general StaticGradientBg component

---
+++ src/components/StaticGradientBg.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {
+  children?: React.ReactNode;
+  styles?: string;
+};
+
+export default function StaticGradientBg({ children, styles }: Props) {
+  return (
+    <div className={"background-all w-full h-screen " + styles}>{children}</div>
+  );
+}
--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,14 +1,15 @@
 // type Props = {};
 
+import StaticGradientBg from "../StaticGradientBg";
 import RegisterForm from "./RegisterForm";
 
 export default function Register() {
   return (
-    <div className="background-all w-full h-screen">
+    <StaticGradientBg>
       <div className="content-wrapper m-6 p-8 pb-12 bg-outer-space-half rounded-md">
         <h1 className="font-redhat text-3xl ml-10">Dołącz do OmniKina</h1>
         <RegisterForm />
       </div>
-    </div>
+    </StaticGradientBg>
   );
 }
