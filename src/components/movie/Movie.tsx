4bd312bbb52ef89cdbb93ba485566467ce022bb3
Michał Bunda <m.bunda.721@studms.ug.edu.pl>
2024-01-18 12:29:36 +0100

    created gallery component

--- src/components/movie/Movie.tsx
+++ src/components/movie/Movie.tsx
@@ -10,11 +10,10 @@ import { ScreeningType } from '../../utils/screeningsUtils';
 import AddScreening from './AddScreening';
 import { AdminContext } from '../../contexts/AdminContext';
 import { fetchScreenings } from '../../utils/fetchScreenings';
-import AddPhoto from './AddPhoto';
 import YoutubeEmbed from './YoutubeEmbed';
 import AddTrailer from './AddTrailer';
 import { getYoutubeEmbedFromUrl } from '../../utils/getYoutubeEmbed';
-import Image from './Image';
+import Gallery from './Gallery';
 
 export default function Movie() {
 	const { movieId } = useParams();
@@ -180,33 +179,15 @@ export default function Movie() {
 						)}
 					</div>
 				)}
-
-				<div className='w-11/12 mt-8 self-center border-b-2 border-b-slate-500'>
-					<p className='ml-10 p-2 font-medium text-xl'>Galeria</p>
-				</div>
-				{/* <div className='galleryWrapper w-full grid auto-cols-auto grid-flow-col-dense auto-rows-[12em] gap-2 mt-6 ml-8'> */}
-				<div className='galleryWrapper w-full flex flex-wrap gap-2 mt-6 ml-8'>
-					{movie?.galleryPhotoUrls.map((photoUrl, index) => (
-						<Image
-							key={index}
-							setHasChanged={setContentHasChanged}
-							src={photoUrl}
-							movieId={movieId || ''}
-							className='h-48'
-						/>
-					))}
-					{adminContext?.isAdmin && (
-						<p
-							onClick={() => setShowAddPhoto(true)}
-							className='cursor-pointer self-center bg-outer-space-quarter border border-magnolia hover:border-rosered hover:text-rosered hover:bg-transparent transition-colors duration-100 px-6 py-3 w-fit rounded-md text-lg text-center align-middle'
-						>
-							+
-						</p>
-					)}
-					{showAddPhoto && (
-						<AddPhoto setShow={setShowAddPhoto} movieId={movieId} />
-					)}
-				</div>
+				{movie && movieId && (
+					<Gallery
+						movie={movie}
+						movieId={movieId}
+						showAddPhoto={showAddPhoto}
+						setShowAddPhoto={setShowAddPhoto}
+						setContentHasChanged={setContentHasChanged}
+					/>
+				)}
 			</div>
 		</StaticGradientBg>
 	);
